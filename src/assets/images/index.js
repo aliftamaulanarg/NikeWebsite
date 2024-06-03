4488741316c778c5a86cad7ab1c284f99584e004
Alifta Maulana Rayhan Giraldi <159220486+aliftamaulanarg@users.noreply.github.com>
2024-06-03 15:11:31 +0700

    update index js and hero jsx

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,8 @@
 import bigShoe1 from './big-shoe1.png'
 import bigShoe2 from './big-shoe2.png'
 import bigShoe3 from './big-shoe3.png'
+import cardBNI1 from './cardbni1.jpg'
+
 
 import customer1 from './customer1.jpeg'
 import customer2 from './customer2.svg'
@@ -25,6 +27,7 @@ export {
     bigShoe1,
     bigShoe2,
     bigShoe3,
+    cardBNI1,
 
     customer1,
     customer2,
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
 import { statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import { cardBNI1 } from "../assets/images";
 
 const Hero = () => {
   return (
@@ -33,6 +35,19 @@ const Hero = () => {
           </div>
       </div>
 
+            <div className="relative flex-1 flex justify-center 
+            items-center 
+            xl:min-h-screen max-xl:py-40 bg-primary 
+            bg-hero bg-cover bg-center" >
+              <img
+              src={bigShoe1}
+              alt="shoe collection"
+              width={610}
+              height={500}
+              className="object-contain-relative z-10">
+
+              </img>
+            </div>
     </section>
     )
 }
